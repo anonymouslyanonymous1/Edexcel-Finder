31ec4707f03e3e45f3c8bb2cf82598264857a428
Anonymously Anonymous <99310000+anonymouslyanonymous1@users.noreply.github.com>
2025-04-17 00:36:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -36,14 +36,10 @@ def results():
                 for word in Search:
                     word = re.sub(f"[{re.escape(string.punctuation)}]", "", word)
                     searchh = searchh + f"content:{word} OR "
-                print(searchh)
                 query = parser.parse(f'subject:"{subject}" AND unit:"Unit {unit}" AND module:"{module}" AND ({searchh})') # Turning into a query object
                 results = searcher.search(query, limit=40)
-                print(query)
-                print(f"{len(results)} found")
                 hits = len(results)
                 for result in results:
-                    print(result["year"], result.score)
                     pdf_path = f'static/Papers/{result["subject"]}/{result["unit"]}/{result["year"]}.txt'
                     QP = open(pdf_path, "r", encoding="utf-8")
                     content = QP.read()    
@@ -72,14 +68,39 @@ def results():
                 query = parser.parse(f'subject:"{subject}" AND unit:"{module}" AND module:"Unit {unit}" AND content:"{search}"') # Turning into a query object
                 results = searcher.search(query, limit=40)
                 hits = len(results)
-                print(f"len(results) found")
                 for result in results:
                     pdf_path = f'static/Papers/{result["subject"]}/{result["unit"]}/{result["module"]}/{result["year"]}.txt'
                     QP = open(pdf_path, "r", encoding="utf-8")
                     content = QP.read()    
                     content = re.sub(" ", "", content)
                     search = re.sub(" ", "", search)
-                    if content.lower().find(search) != -1:
+                    if len(search.split()) == 1:
+                        begin = 0
+                        instances = [] # Page
+                        while True:
+                            if content.lower().find(search, begin) == -1:
+                                break
+                            if content.lower().find(search, begin) != -1:
+                                for index in range(content.lower().find(search, begin), len(content)):
+                                    if content[index: index + 15] == "-----EndofPage:":
+                                        page = content[index + 15]
+                                        if content[index + 16] != "-":
+                                            page = page + content[index + 16]
+                                            break
+                                        else:
+                                            break
+                                page = int(page) + 1
+                                begin = len(search) + content.lower().find(search, begin)
+                                if page != 1 and (page not in instances):
+                                    instances.append(page)
+                        doc = fitz.open(re.sub(".txt",".pdf",pdf_path))
+                        for instance in instances:
+                            Page = doc.load_page(instance-1)
+                            pix = Page.get_pixmap(dpi=160)
+                            pix.save(f'static/images/{result["year"]} pg{instance}.png')
+                            resultss.append([result["year"], instance, f'static/images/{result["year"]} pg{instance}.png', re.sub(".txt",".pdf",pdf_path), re.sub(".txt", " MS.pdf", pdf_path)])
+                        hits = len(resultss)
+                    elif len(search.split()) != 1 and content.lower().find(search) != -1:
                         for index in range(content.lower().find(search), len(content)):
                             if content[index: index + 15] == "-----EndofPage:":
                                 page = content[index + 15]
@@ -88,12 +109,12 @@ def results():
                                     break
                                 else:
                                     break                   
-                    page = int(page) + 1
-                    doc = fitz.open(re.sub(".txt",".pdf",pdf_path))
-                    Page = doc.load_page(page-1)
-                    pix = Page.get_pixmap(dpi=160)
-                    pix.save(f'static/images/{result["year"]} pg{page}.png')
-                    resultss.append([result["year"], page, f'static/images/{result["year"]} pg{page}.png', re.sub(".txt",".pdf",pdf_path), re.sub(".txt", " MS.pdf", pdf_path)])
+                        page = int(page) + 1
+                        doc = fitz.open(re.sub(".txt",".pdf",pdf_path))
+                        Page = doc.load_page(page-1)
+                        pix = Page.get_pixmap(dpi=160)
+                        pix.save(f'static/images/{result["year"]} pg{page}.png')
+                        resultss.append([result["year"], page, f'static/images/{result["year"]} pg{page}.png', re.sub(".txt",".pdf",pdf_path), re.sub(".txt", " MS.pdf", pdf_path)])
         else:
             if search[0] == '"' and search[len(search)-1]=='"' and len(search.split()) != 1:
                 Search = search.split()
@@ -101,21 +122,43 @@ def results():
                 for word in Search:
                     word = re.sub(f"[{re.escape(string.punctuation)}]", "", word)
                     searchh = searchh + f"content:{word} OR "
-                print(searchh)
                 query = parser.parse(f'subject:"{subject}" AND unit:"Unit {unit}" AND module:"{module}" AND ({searchh})') # Turning into a query object
                 results = searcher.search(query, limit=40)
-                print(query)
-                print(f"{len(results)} found")
                 hits = len(results)
                 for result in results:
-                    print(result["year"], result.score)
                     pdf_path = f'static/Papers/{result["subject"]}/{result["unit"]}/{result["year"]}.txt'
                     QP = open(pdf_path, "r", encoding="utf-8")
                     content = QP.read()    
                     content = re.sub(" ", "", content)
                     search = re.sub(" ", "", search)
                     search = re.sub('"',"", search)
-                    if content.lower().find(search) != -1:
+                    if len(search.split()) == 1:
+                        begin = 0
+                        instances = [] # Page
+                        while True:
+                            if content.lower().find(search, begin) == -1:
+                                break
+                            if content.lower().find(search, begin) != -1:
+                                for index in range(content.lower().find(search, begin), len(content)):
+                                    if content[index: index + 15] == "-----EndofPage:":
+                                        page = content[index + 15]
+                                        if content[index + 16] != "-":
+                                            page = page + content[index + 16]
+                                            break
+                                        else:
+                                            break
+                                page = int(page) + 1
+                                begin = len(search) + content.lower().find(search, begin)
+                                if page != 1 and (page not in instances):
+                                    instances.append(page)
+                        doc = fitz.open(re.sub(".txt",".pdf",pdf_path))
+                        for instance in instances:
+                            Page = doc.load_page(instance-1)
+                            pix = Page.get_pixmap(dpi=160)
+                            pix.save(f'static/images/{result["year"]} pg{instance}.png')
+                            resultss.append([result["year"], instance, f'static/images/{result["year"]} pg{instance}.png', re.sub(".txt",".pdf",pdf_path), re.sub(".txt", " MS.pdf", pdf_path)])
+                        hits = len(resultss)
+                    elif len(search.split()) != 1 and content.lower().find(search) != -1:
                         for index in range(content.lower().find(search), len(content)):
                             if content[index: index + 15] == "-----EndofPage:":
                                 page = content[index + 15]
@@ -140,15 +183,42 @@ def results():
                 query = parser.parse(f'subject:"{subject}" AND unit:"Unit {unit}" AND module:"{module}" AND content:"{search}"') # Turning into a query object
                 results = searcher.search(query, limit=40)
                 hits = len(results)
-                print(f"{len(results)} found")
                 for result in results:
-                    print(result["year"], result.score)
                     pdf_path = f'static/Papers/{result["subject"]}/{result["unit"]}/{result["year"]}.txt'
                     QP = open(pdf_path, "r", encoding="utf-8")
                     content = QP.read()    
                     content = re.sub(" ", "", content)
                     search = re.sub(" ", "", search)
-                    if content.lower().find(search) != -1:
+                    if len(search.split()) == 1:
+                        begin = 0
+                        instances = [] # Page
+                        while True:
+                            if content.lower().find(search, begin) == -1:
+                                break
+                            if content.lower().find(search, begin) != -1:
+                                for index in range(content.lower().find(search, begin), len(content)):
+                                    if content[index: index + 15] == "-----EndofPage:":
+                                        page = content[index + 15]
+                                        if content[index + 16] != "-":
+                                            page = page + content[index + 16]
+                                            break
+                                        else:
+                                            break
+                                if result["year"] == "Sample Assessment":
+                                    page = int(page) + 2
+                                else:
+                                    page = int(page) + 1
+                                begin = len(search) + content.lower().find(search, begin)
+                                if page != 1 and (page not in instances):
+                                    instances.append(page)
+                        doc = fitz.open(re.sub(".txt",".pdf",pdf_path))
+                        for instance in instances:
+                            Page = doc.load_page(instance-1)
+                            pix = Page.get_pixmap(dpi=160)
+                            pix.save(f'static/images/{result["year"]} pg{instance}.png')
+                            resultss.append([result["year"], instance, f'static/images/{result["year"]} pg{instance}.png', re.sub(".txt",".pdf",pdf_path), re.sub(".txt", " MS.pdf", pdf_path)])
+                        hits = len(resultss)
+                    elif len(search.split()) != 1 and content.lower().find(search) != -1:
                         for index in range(content.lower().find(search), len(content)):
                             if content[index: index + 15] == "-----EndofPage:":
                                 page = content[index + 15]
@@ -161,11 +231,11 @@ def results():
                             page = int(page) + 2
                         else:
                             page = int(page) + 1
-                    doc = fitz.open(re.sub(".txt",".pdf",pdf_path))
-                    Page = doc.load_page(page-1)
-                    pix = Page.get_pixmap(dpi=160)
-                    pix.save(f'static/images/{result["year"]} pg{page}.png')
-                    resultss.append([result["year"], page, f'static/images/{result["year"]} pg{page}.png', re.sub(".txt",".pdf",pdf_path), re.sub(".txt", " MS.pdf", pdf_path)])
+                        doc = fitz.open(re.sub(".txt",".pdf",pdf_path))
+                        Page = doc.load_page(page-1)
+                        pix = Page.get_pixmap(dpi=160)
+                        pix.save(f'static/images/{result["year"]} pg{page}.png')
+                        resultss.append([result["year"], page, f'static/images/{result["year"]} pg{page}.png', re.sub(".txt",".pdf",pdf_path), re.sub(".txt", " MS.pdf", pdf_path)])
         return render_template("results.html", results = resultss, hits = hits)
 @app.errorhandler(500)
 def internal_error(error):
